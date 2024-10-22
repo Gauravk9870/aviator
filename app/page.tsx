5c2760fbf38902ed51fe61e907cb564e366ae057
Gauravk9870 <gauravk968570@gmail.com>
2024-10-22 23:43:45 +0530

    fix iframe size

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,9 @@ import Avitor from "@/components/layout/Avitor";
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col bg-[#0e0e0e]">
-      <div className="flex-1">
-        <Avitor />
-      </div>
-      <div className="flex-1">
-        <BetControl />
-      </div>
+    <main className=" h-[calc(100vh-119.5px)] flex flex-col bg-[#0e0e0e]  justify-between">
+      <Avitor />
+      <BetControl />
     </main>
   );
 }
