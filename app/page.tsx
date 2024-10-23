4a407a96bd12e2247039d7643ab108f2a9471658
RahulRawat07 <rahulrawat965455@gmail.com>
2024-10-23 22:52:03 +0530

    ERR:padding

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-// import Avitor from "@/components/layout/Avitor"
+// import Avitor from "@/components/layout/Avitor";
 import BetControl from "@/components/layout/BetControl";
 import Avitor from "@/components/layout/Avitor";
 
 export default function Home() {
   return (
-    <main className=" lg:h-[calc(100vh-119.5px)] flex flex-col bg-[#0e0e0e] justify-between lg:p-2">
-      <Avitor />
-      <BetControl />
+    <main className="lg:h-[calc(100vh-119.5px)] flex flex-col bg-[#0e0e0e]  p-2">
+      <div className="flex-2 flex-grow-[2]">
+        <Avitor />
+      </div>
+      <div className="flex-1 flex-grow">
+        <BetControl />
+      </div>
     </main>
   );
 }
