208ae8a594d536a8a7374d4d213d5a992b8dcdf9
Gauravk9870 <gauravk968570@gmail.com>
2024-10-27 22:49:43 +0530

    recover autoplay

--- components/layout/BetControl.tsx
+++ components/layout/BetControl.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import React, { useState, FC } from "react";
-import { Minus, Plus } from "lucide-react";
+import { Minus, Plus, X } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const BetSection: FC<{
   isBetting: boolean;
@@ -10,10 +13,11 @@ const BetSection: FC<{
   setBetAmount: React.Dispatch<React.SetStateAction<number>>;
 }> = ({ isBetting, handleBet, betAmount, setBetAmount }) => {
   const handleIncrement = () => setBetAmount((prev) => prev + 1);
-  const handleDecrement = () => setBetAmount((prev) => (prev > 1 ? prev - 1 : 1));
+  const handleDecrement = () =>
+    setBetAmount((prev) => (prev > 1 ? prev - 1 : 1));
 
-  const buttonClass = isBetting 
-    ? "bg-[#141516] text-[#ffffff80] cursor-not-allowed opacity-50" 
+  const buttonClass = isBetting
+    ? "bg-[#141516] text-[#ffffff80] cursor-not-allowed opacity-50"
     : "bg-[#141516] text-white";
 
   return (
@@ -60,13 +64,75 @@ const BetSection: FC<{
             </span>
           )}
           <button
-            className={`w-[160px] flex items-center justify-center rounded-2xl border shadow-inner ${isBetting ? 'bg-red-600' : 'bg-[#28a909]'} h-20`}
+            className={`w-[160px] flex items-center justify-center rounded-2xl border shadow-inner ${
+              isBetting ? "bg-red-600" : "bg-[#28a909]"
+            } h-20`}
             onClick={handleBet}
           >
             <span className="text-lg font-normal uppercase text-shadow text-white">
               {isBetting ? "Cancel" : "Bet"}
             </span>
           </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AutoSection: FC<{
+  isBetting: boolean;
+  handleBet: () => void;
+  betAmount: number;
+  setBetAmount: React.Dispatch<React.SetStateAction<number>>;
+}> = ({ isBetting, handleBet, betAmount, setBetAmount }) => {
+  const [autoCashOut, setAutoCashOut] = useState<boolean>(false);
+  const [autoCashOutAmount, setAutoCashOutAmount] = useState<number>(2.0);
+
+  const handleClearAutoCashOut = () => {
+    setAutoCashOutAmount(0);
+  };
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <BetSection
+        isBetting={isBetting}
+        handleBet={handleBet}
+        betAmount={betAmount}
+        setBetAmount={setBetAmount}
+      />
+      <div className="flex items-center gap-2">
+        <div className=" flex-1 flex items-center justify-center">
+          <button className="w-4/5 text-sm bg-[#1d7aca] border border-[#46c0f2] rounded-3xl text-white uppercase py-1 px-4">
+            Auto Play
+          </button>
+        </div>
+        <div className="flex-1 flex items-center justify-center rounded-3xl px-3 py-2 gap-2">
+          <div className="flex items-center gap-2">
+            <span className="text-[#9ea0a3] text-sm">Auto Cash Out</span>
+            <Switch
+              checked={autoCashOut}
+              onCheckedChange={setAutoCashOut}
+              className="border-2 border-gray-600 bg-transparent data-[state=checked]:border-[#60ae05] data-[state=checked]:bg-[#229607] data-[state=unchecked]:bg-transparent"
+            />
+          </div>
+
+          <div className="relative">
+            <Input
+              type="number"
+              value={autoCashOutAmount}
+              onChange={(e) => setAutoCashOutAmount(Number(e.target.value))}
+              className="w-16 text-white border-none text-right h-auto bg-[#000000b3] outline-none rounded-3xl hide-spin-buttons pr-8 py-1"
+              disabled={!autoCashOut}
+            />
+            {autoCashOut && (
+              <button
+                onClick={handleClearAutoCashOut}
+                className="absolute right-2 top-1/2 transform -translate-y-1/2 text-white hover:text-gray-200"
+              >
+                <X size={16} />
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -90,25 +156,96 @@ const BetControl: FC = () => {
 
   return (
     <div className="flex flex-col lg:flex-row justify-end gap-2 pt-2 pb-2 lg:pb-0">
-     <div className={`flex-1 px-4 lg:px-10 py-4 rounded-md bg-[#222222] ${
-  isBetting1 ? "border-2 border-red-500" : "border border-transparent"
-}`}>
-        <BetSection
-          isBetting={isBetting1}
-          handleBet={handleBet1}
-          betAmount={betAmount1}
-          setBetAmount={setBetAmount1}
-        />
+      <div className="flex-1 px-4 lg:px-10 py-4 rounded-md bg-[#222222] ">
+        <Tabs
+          defaultValue="bet"
+          className="flex flex-col items-center justify-center gap-2"
+        >
+          <TabsList className="bg-[#141516] w-full max-w-[200px] rounded-3xl h-auto p-0">
+            <TabsTrigger
+              value="bet"
+              className="w-1/2 rounded-3xl data-[state=active]:bg-[#2c2d30] text-white data-[state=active]:text-white py-0.5 px-4 flex-1"
+            >
+              Bet
+            </TabsTrigger>
+            <TabsTrigger
+              value="auto"
+              className="w-1/2 rounded-3xl data-[state=active]:bg-[#2c2d30] text-white data-[state=active]:text-white py-0.5 px-4 flex-1"
+            >
+              Auto
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="bet" className="w-full">
+            <div
+              className={`flex-1 px-4 lg:px-10 py-4 rounded-md bg-[#222222] ${
+                isBetting1
+                  ? "border-2 border-red-500"
+                  : "border border-transparent"
+              }`}
+            >
+              <BetSection
+                isBetting={isBetting1}
+                handleBet={handleBet1}
+                betAmount={betAmount1}
+                setBetAmount={setBetAmount1}
+              />
+            </div>
+          </TabsContent>
+          <TabsContent value="auto" className="w-full">
+            <AutoSection
+              isBetting={isBetting2}
+              handleBet={handleBet2}
+              betAmount={betAmount2}
+              setBetAmount={setBetAmount2}
+            />
+          </TabsContent>
+        </Tabs>
       </div>
-      <div className={`flex-1 px-4 lg:px-10 py-4 rounded-md bg-[#222222] ${
-  isBetting2 ? "border-2 border-red-500" : "border border-transparent"
-}`}>
-        <BetSection
-          isBetting={isBetting2}
-          handleBet={handleBet2}
-          betAmount={betAmount2}
-          setBetAmount={setBetAmount2}
-        />
+
+      <div className="flex-1 px-4 lg:px-10 py-4 rounded-md bg-[#222222] ">
+        <Tabs
+          defaultValue="bet"
+          className="flex flex-col items-center justify-center gap-2"
+        >
+          <TabsList className="bg-[#141516] w-full max-w-[200px] rounded-3xl h-auto p-0">
+            <TabsTrigger
+              value="bet"
+              className="w-1/2 rounded-3xl data-[state=active]:bg-[#2c2d30] text-white data-[state=active]:text-white py-0.5 px-4 flex-1"
+            >
+              Bet
+            </TabsTrigger>
+            <TabsTrigger
+              value="auto"
+              className="w-1/2 rounded-3xl data-[state=active]:bg-[#2c2d30] text-white data-[state=active]:text-white py-0.5 px-4 flex-1"
+            >
+              Auto
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="bet" className="w-full">
+            <div
+              className={`flex-1 px-4 lg:px-10 py-4 rounded-md bg-[#222222] ${
+                isBetting2
+                  ? "border-2 border-red-500"
+                  : "border border-transparent"
+              }`}
+            >
+              <BetSection
+                isBetting={isBetting2}
+                handleBet={handleBet2}
+                betAmount={betAmount2}
+                setBetAmount={setBetAmount2}
+              />
+            </div>
+          </TabsContent>
+          <TabsContent value="auto" className="w-full">
+            <AutoSection
+              isBetting={isBetting2}
+              handleBet={handleBet2}
+              betAmount={betAmount2}
+              setBetAmount={setBetAmount2}
+            />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
