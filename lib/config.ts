d103f9e0bbada755410648e5a8d0d0438c2ba762
Vaibhav Rohilla <vaibhavrohilla2000@gmail.com>
2024-11-09 16:40:58 +0530

    Changed Config

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,7 @@
 const _config = {
     server: process.env.NEXT_PUBLIC_SERVER_URL,
     ws: process.env.NEXT_PUBLIC_WS_URL,
+    userToken: process.env.NEXT_PUBLIC_USERTOKEN,
   };
   
   export const config = Object.freeze(_config);
