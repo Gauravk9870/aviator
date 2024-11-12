55eb4b5c7dcdbc47480de144fa93a28ea87ae93b
Vaibhav Rohilla <66160559+VaibhavRohilla@users.noreply.github.com>
2024-11-12 23:56:55 +0530

    Update Avitor.tsx

--- components/layout/Avitor.tsx
+++ components/layout/Avitor.tsx
@@ -41,7 +41,7 @@ export default function Avitor({ setIsLoading }: AvitorProps) {
     <div className="h-[200px] w-full relative border border-[#6666664b] rounded-2xl lg:h-full">
       <iframe
         id="iframeID"
-        src="http://localhost:8080/"
+        src="https://elaborate-monstera-c8852a.netlify.app/"
         className="h-full w-full rounded-2xl"
         onLoad={handleLoad}
         style={{
